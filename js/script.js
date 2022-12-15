ac27a721382ae2de5b39db73ef584a9a7b0269b0
gordonk <gpkarlsson72@gmail.com>
2022-12-15 15:01:39 -0600

    added html page for high scores, worked on js

--- js/script.js
+++ js/script.js
@@ -11,16 +11,24 @@ const buttonStart = document.getElementById('buttonStart');
 const buttonNext = document.getElementById('nextButton');
 const questionsContainerEl = document.getElementById('questionsDiv');
 
-const questionEl = document.getElementById('question')
-const answerButtonEl = document.getElementById('answerButton')
+const questionEl = document.getElementById('question');
+const answerButtonEl = document.getElementById('answerButton');
+var score = 0;
+var timer = 45;
 
-let randomQuestions, currentQuestionIndex
+let randomQuestions, currentQuestionIndex;
 
-buttonStart.addEventListener('click', startQuiz)
+buttonStart.addEventListener('click', startQuiz);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    nextQuestion()
+});
 
+//Start Quiz
 function startQuiz() {
 console.log('started');
 buttonStart.classList.add('hide');
+highScore.classList.add('hide');
 randomQuestions = questions.sort(() => Math.random() - .5)
 currentQuestionIndex = 0
 questionsContainerEl.classList.remove('hide');
@@ -32,40 +40,105 @@ function nextQuestion() {
     displayQuestion(randomQuestions[currentQuestionIndex])
 }
 
+// Displays quiz question
 function displayQuestion(question) {
     questionEl.innerText = question.question
     question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
         if (answer.correct) {
             button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', choiceAnswer)
-        answerButtonEl.appendChild(button)
-    })
-} 
+        };
+        button.addEventListener('click', choiceAnswer);
+        answerButtonEl.appendChild(button);
+    });
+};
 
+// Function to show whether chosen answer is correct
+function choiceAnswer(e) {
+    const buttonSelected = e.target
+    const correct = buttonSelected.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (randomQuestions.length > currentQuestionIndex + 1) {
+    buttonNext.classList.remove('hide')
+    } else {
+        buttonStart.innerText = 'Restart'
+        buttonStart.classList.remove('hide');
+    }
+}
+
+// sets correct or wrong class depending on whether answer was correct or not
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+// resets correct or wrong class for the next question
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+// resets quiz UI between each question 
 function stateReset() {
+    clearStatusClass(document.body)
    buttonNext.classList.add('hide')
    while (answerButtonEl.firstChild) {
     answerButtonEl.removeChild(answerButtonEl.firstChild)
    }
 }
 
-
-function answer(e) {
-
-}
-
+// questions for quiz
 const questions = [
     {
-        question: 'What song did the Grateful Dead play the most amount of times live?',
+        question: 'What is the proper way to name things in JavaScript?',
         answers: [
-            {text: 'Drums', correct: true},
-            {text: 'Sugar Magnolia', correct: false},
-            {text: 'Space', correct: false},
-            {text: 'Friend of the Devil', correct: false}
+            {text: 'camelCase', correct: true},
+            {text: 'all lower case', correct: false},
+            {text: 'ALL CAPS', correct: false},
+            {text: 'Nospaces', correct: false}
+        ]
+    },
+
+    {
+        question: 'What HTML element do we put the JavaScript link into?',
+        answers: [
+            {text: '<script>', correct: true},
+            {text: '<link>', correct: false},
+            {text: '<head>', correct: false},
+            {text: '<p>', correct: false},
+        ]
+    },
+
+    {
+        question: 'What do you use to display messages in the console?',
+        answers: [
+            {text: 'print.log()', correct: false},
+            {text: 'write()', correct: false},
+            {text: 'console.log()', correct: true},
+            {text: 'Phish', correct: false},
+        ]
+    },
+
+    {
+        question: 'Who invented JavaScript?',
+        answers: [
+            {text: 'Bob Weir', correct: false},
+            {text: 'Brendan Eich', correct: true},
+            {text: 'Steve Jobs', correct: false},
+            {text: 'Bill Gates', correct: false},
         ]
     }
-]
+];
+
+if (answerChoice.correct == true) {
+    score++;
+}
